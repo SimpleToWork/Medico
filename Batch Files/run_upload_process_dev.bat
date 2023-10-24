8ba1a8be8865a73f7becf972afc56c00138be3f5
Ricky Schweky <Ricky@Simpletowork.com>
2023-10-24 13:11:53 -0400

    updates

---
+++ Batch Files/run_upload_process_dev.bat
@@ -0,0 +1,4 @@
+cd C:\
+"C:\Users\%USERNAME%\Desktop\New Projects\Medico\Medico\venv\scripts\python.exe" "C:\Users\%USERNAME%\Desktop\New Projects\Medico\Medico\Python Files\program_manager.py" "development" "Upload Process"
+
+cmd/k
