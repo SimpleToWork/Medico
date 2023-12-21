5b9140a11fb724a58abadfe34c011654bf62f4cb
Ricky Schweky <Ricky@Simpletowork.com>
2023-12-21 04:40:51 -0500

    updates

--- Batch Files/run_merge_process.bat
+++ Batch Files/run_merge_process.bat
@@ -1,4 +1,4 @@
 cd C:\
 "C:\Users\%USERNAME%\Desktop\New Projects\Medico\Medico\venv\scripts\python.exe" "C:\Users\%USERNAME%\Desktop\New Projects\Medico\Medico\Python Files\program_manager.py" "production" "Merge Files"
 
-cmd/c
+cmd/k
