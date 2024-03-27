1d6c36e74a5d01513ecbc508c1c2bf92cfb6c6a7
Ricky Schweky <Ricky@Simpletowork.com>
2024-03-27 06:51:16 -0400

    updates

---
+++ Batch Files/run_email_diagnostic.bat
@@ -0,0 +1,4 @@
+cd C:\
+"C:\Users\%USERNAME%\Desktop\New Projects\Medico\Medico\venv\scripts\python.exe" "C:\Users\%USERNAME%\Desktop\New Projects\Medico\Medico\Python Files\program_manager.py" "production" "Email Diagnostic"
+
+cmd/c
