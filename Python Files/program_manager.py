4650356749330adca9819e06b42e5f1d458bea2e
Ricky Schweky <Ricky@Simpletowork.com>
2024-05-11 14:10:09 -0400

    updates

--- Python Files/program_manager.py
+++ Python Files/program_manager.py
@@ -33,8 +33,8 @@ def run_program(environment, function_to_run):
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        environment = 'development'
-        function_to_run = 'Email Diagnostic'
+        environment = 'production'
+        function_to_run = 'Upload Process'
     else:
         environment = sys.argv[1]
         function_to_run = sys.argv[2]
